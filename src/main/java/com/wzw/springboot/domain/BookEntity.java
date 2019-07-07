331a58b817e5587802db2032ea9877f7e2be54d6
Joker_wang <18320491930@163.com>
2019-07-07 15:34:38 +0800

    select

--- src/main/java/com/wzw/springboot/domain/BookEntity.java
+++ src/main/java/com/wzw/springboot/domain/BookEntity.java
@@ -14,61 +14,35 @@ public class BookEntity implements Serializable {
     private String pirce;
 
 
-
-    /**
-     * @return bid
-     */
     public Integer getBid() {
         return bid;
     }
 
-    /**
-     * @param bid
-     */
     public void setBid(Integer bid) {
         this.bid = bid;
     }
 
-    /**
-     * @return bname
-     */
     public String getBname() {
         return bname;
     }
 
-    /**
-     * @param bname
-     */
     public void setBname(String bname) {
-        this.bname = bname == null ? null : bname.trim();
+        this.bname = bname;
     }
 
-    /**
-     * @return detail
-     */
     public String getDetail() {
         return detail;
     }
 
-    /**
-     * @param detail
-     */
     public void setDetail(String detail) {
-        this.detail = detail == null ? null : detail.trim();
+        this.detail = detail;
     }
 
-    /**
-     * @return pirce
-     */
     public String getPirce() {
         return pirce;
     }
 
-    /**
-     * @param pirce
-     */
     public void setPirce(String pirce) {
-        this.pirce = pirce == null ? null : pirce.trim();
+        this.pirce = pirce;
     }
-
 }
---
+++ src/main/java/com/wzw/springboot/domain/ListId.java
@@ -0,0 +1,23 @@
+package com.wzw.springboot.domain;
+
+import java.util.List;
+
+/**
+ * @Author: Joker_wang
+ * @Date: 2019/7/7 13:58
+ * @Version 1.0.0
+ */
+public class ListId {
+    List<String> idLIst;
+
+    public ListId() {
+    }
+
+    public List<String> getIdLIst() {
+        return idLIst;
+    }
+
+    public void setIdLIst(List<String> idLIst) {
+        this.idLIst = idLIst;
+    }
+}
